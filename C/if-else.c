241b0f4b76e0069b55742ee630595b4706820a27
RonaldMcDonald <maxkoch@tsn.at>
2022-10-12 17:39:28 +0200

    A

--- C/if-else.c
+++ C/if-else.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int firstNumber;
-    int secondNumber;
+    float ffirstNumber;
+    float fsecondNumber;
 
     printf("Enter first number: ");
-    scanf("%d", &firstNumber);
+    scanf("%d", &ffirstNumber);
     printf("Enter second number: ");
-    scanf("%d", &secondNumber);
+    scanf("%d", &fsecondNumber);
 
-    if (firstNumber > secondNumber) {
-        printf("Your first number %d is greater than your second number %d.", firstNumber, secondNumber);
-    } else if (firstNumber < secondNumber) {
-        printf("Your second number %d is greater than your first number %d", secondNumber, firstNumber);
+    if (ffirstNumber > fsecondNumber) {
+        printf("Your first number %d is greater than your second number %d.", ffirstNumber, fsecondNumber);
+    } else if (ffirstNumber < fsecondNumber) {
+        printf("Your second number %d is greater than your first number %d", fsecondNumber, ffirstNumber);
     } else {
         printf("Both numbers are equal");
     }
