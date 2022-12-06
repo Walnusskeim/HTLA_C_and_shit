47cb3b605822fad4c9e4c5f4903b0979008a66c6
RonaldMcDonald <maxkoch@tsn.at>
2022-12-06 20:16:12 +0100

    Ned vollständig lol koan Bock

---
+++ C/Rechtek_runner.c
@@ -0,0 +1,17 @@
+//
+// Created by MAKO on 30/11/2022.
+//
+
+void  RechteckWanderung(iZeileOben, iSpalteLinks, iZeileUnten, iSpalteRechts, cZeichen, iDelay);
+{
+
+
+}
+
+int main ()
+{
+    // allgemein
+    // RechteckWanderung (iZeileOben,iSpalteLinks,iZeileUnten,iSpalteRechts,cZeichen,iDelay);
+    // Beispiel-Aufruf
+
+    RechteckWanderung (3,20,20,60,'+',50);
